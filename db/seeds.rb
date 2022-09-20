4c1ec39a47fda1121923e7e14c6b7d227cd88c66
Mohd Akibuddin <akeebudeen786@gmail.com>
2022-09-20 09:27:13 +0530

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# users = User.create([
+#   { email: 'akeebudeen1786@gmail.com', password: "zxczxc" },
+#   { email: 'akeebudeen2786@gmail.com', password: "zxczxc"  },
+#   { email: 'akeebudeen3786@gmail.com', password: "zxczxc"  },
+#   { email: 'akeebudeen4786@gmail.com', password: "zxczxc"  },
+#   { email: 'akeebudeen5786@gmail.com', password: "zxczxc"  }
+# ])
+
+# users[4].events.create([
+#   { title: 'Yabba Dabba Doo Party', location: 'Flintstone Cave', date: '2020-05-01', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis mi vitae ex pharetra lacinia. In feugiat ante sit amet nisl consequat lobortis. Praesent sit amet enim sit amet felis malesuada mattis. Pellentesque fringilla placerat odio sit amet convallis. Proin molestie sem eu magna elementum, quis laoreet est tempus. Integer in tempor nulla, at mattis erat. Etiam consequat dui ut nibh dapibus tempus. Curabitur semper luctus massa, vel scelerisque turpis accumsan ultricies.' },
+#   { title: 'Brontosaurus BBQ Evening', location: 'Jurassic Park', date: '2020-08-23', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis mi vitae ex pharetra lacinia. In feugiat ante sit amet nisl consequat lobortis. Praesent sit amet enim sit amet felis malesuada mattis. Pellentesque fringilla placerat odio sit amet convallis. Proin molestie sem eu magna elementum, quis laoreet est tempus. Integer in tempor nulla, at mattis erat. Etiam consequat dui ut nibh dapibus tempus. Curabitur semper luctus massa, vel scelerisque turpis accumsan ultricies.' },
+#   { title: 'Bronto-burger Cooking Class', location: 'Bronto King', date: '2024-04-24', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis mi vitae ex pharetra lacinia. In feugiat ante sit amet nisl consequat lobortis. Praesent sit amet enim sit amet felis malesuada mattis. Pellentesque fringilla placerat odio sit amet convallis. Proin molestie sem eu magna elementum, quis laoreet est tempus. Integer in tempor nulla, at mattis erat. Etiam consequat dui ut nibh dapibus tempus. Curabitur semper luctus massa, vel scelerisque turpis accumsan ultricies.' }
+# ])
+
+# users[5].events.create([
+#   { title: 'Hot dog, hot dog, hot diggity dog', location: 'Mickey Mouse Clubhouse', date: '2023-10-21', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis mi vitae ex pharetra lacinia. In feugiat ante sit amet nisl consequat lobortis. Praesent sit amet enim sit amet felis malesuada mattis. Pellentesque fringilla placerat odio sit amet convallis. Proin molestie sem eu magna elementum, quis laoreet est tempus. Integer in tempor nulla, at mattis erat. Etiam consequat dui ut nibh dapibus tempus. Curabitur semper luctus massa, vel scelerisque turpis accumsan ultricies.' }
+# ])
+
+# users[6].events.create([
+#   { title: 'Mushroom Day', location: 'Wonderland', date: '2020-10-21', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis mi vitae ex pharetra lacinia. In feugiat ante sit amet nisl consequat lobortis. Praesent sit amet enim sit amet felis malesuada mattis. Pellentesque fringilla placerat odio sit amet convallis. Proin molestie sem eu magna elementum, quis laoreet est tempus. Integer in tempor nulla, at mattis erat. Etiam consequat dui ut nibh dapibus tempus. Curabitur semper luctus massa, vel scelerisque turpis accumsan ultricies.' },
+#   { title: 'Cookies Happy Hour', location: 'Wonderland', date: '2025-08-23', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis mi vitae ex pharetra lacinia. In feugiat ante sit amet nisl consequat lobortis. Praesent sit amet enim sit amet felis malesuada mattis. Pellentesque fringilla placerat odio sit amet convallis. Proin molestie sem eu magna elementum, quis laoreet est tempus. Integer in tempor nulla, at mattis erat. Etiam consequat dui ut nibh dapibus tempus. Curabitur semper luctus massa, vel scelerisque turpis accumsan ultricies.' },
+#   { title: '5 o\'clock Tea', location: 'Wonderland', date: '2024-04-24', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis mi vitae ex pharetra lacinia. In feugiat ante sit amet nisl consequat lobortis. Praesent sit amet enim sit amet felis malesuada mattis. Pellentesque fringilla placerat odio sit amet convallis. Proin molestie sem eu magna elementum, quis laoreet est tempus. Integer in tempor nulla, at mattis erat. Etiam consequat dui ut nibh dapibus tempus. Curabitur semper luctus massa, vel scelerisque turpis accumsan ultricies.' }
+# ])
+
+# users[7].events.create([
+#   { title: 'Free Pink Doughnut', location: 'TreeHouse of Horror', date: '2024-7-23', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis mi vitae ex pharetra lacinia. In feugiat ante sit amet nisl consequat lobortis. Praesent sit amet enim sit amet felis malesuada mattis. Pellentesque fringilla placerat odio sit amet convallis. Proin molestie sem eu magna elementum, quis laoreet est tempus. Integer in tempor nulla, at mattis erat. Etiam consequat dui ut nibh dapibus tempus. Curabitur semper luctus massa, vel scelerisque turpis accumsan ultricies.' },
+#   { title: 'Tomacco Degustation', location: 'Kwik-E-Mart', date: '2020-01-10', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis mi vitae ex pharetra lacinia. In feugiat ante sit amet nisl consequat lobortis. Praesent sit amet enim sit amet felis malesuada mattis. Pellentesque fringilla placerat odio sit amet convallis. Proin molestie sem eu magna elementum, quis laoreet est tempus. Integer in tempor nulla, at mattis erat. Etiam consequat dui ut nibh dapibus tempus. Curabitur semper luctus massa, vel scelerisque turpis accumsan ultricies.' }
+# ])
+
+# users[8].events.create([
+#   { title: 'The Krusty Krab\'s Krabby Patty Krush', location: 'Bikini Bottom', date: '2020-10-21', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis mi vitae ex pharetra lacinia. In feugiat ante sit amet nisl consequat lobortis. Praesent sit amet enim sit amet felis malesuada mattis. Pellentesque fringilla placerat odio sit amet convallis. Proin molestie sem eu magna elementum, quis laoreet est tempus. Integer in tempor nulla, at mattis erat. Etiam consequat dui ut nibh dapibus tempus. Curabitur semper luctus massa, vel scelerisque turpis accumsan ultricies.' }
+# ])
